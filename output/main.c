2363a7233a73f966f50e9e31cc5038a221f17d35
Joao Paulo <jp.kourymendoca@gmail.com>
2025-05-22 16:41:04 -0300

    Deleting the functions showControls(), showCredits() and comebackMenu(). Creating the function villageLevel(), generateMapLayout(), drawMap(), movePlayer() and moveEnemie().

--- output/main.c
+++ output/main.c
@@ -6,160 +6,228 @@
 
 // Funções do menu e do jogo
 void showTitle();
-void showControls();
-void showCredits();
-void comebackMenu();
 void generateMap();
+void generateMapLayout(char map[10][10], int width, int height);
+void drawMap(char map[10][10], int width, int height);
+void movePlayer(char map[10][10], char input);
+void moveEnemy(char map[10][10], int width, int height);
+void villageLevel();
+void levelOne();
+void levelTwo();
+void levelThree();
 
+// Variáveis globais
 int selection;
+int enemyX, enemyY;
+int playerX, playerY;
+int levelOneX, levelOneY;
+int levelTwoX, levelTwoY;
+int levelThreeX, levelThreeY;
 
 // Título do jogo
-void showTitle() {
+void showTitle()
+{
     system("cls");
     printf("\n\n\n\n\n\n\n\n");
     printf("\t\t\t\t\t\t ▄████████    ▄████████    ▄████████  ▄█     █▄   ▄█             ▄████████ ▀████    ▐████▀    ▄████████ \n");
     printf("\t\t\t\t\t\t ███    ███   ███    ███   ███    ███ ███     ███ ███            ███    ███   ███▌   ████▀    ███    ███\n");
-    printf("\t\t\t\t\t\t ███    █▀    ███    ███   ███    ███ ███     ███ ███            ███    █▀     ███  ▐███      ███    █▀ \n"); 
-    printf("\t\t\t\t\t\t ███         ▄███▄▄▄▄██▀   ███    ███ ███     ███ ███           ▄███▄▄▄        ▀███▄███▀     ▄███▄▄▄    \n"); 
-    printf("\t\t\t\t\t\t ███        ▀▀███▀▀▀▀▀   ▀███████████ ███     ███ ███          ▀▀███▀▀▀        ████▀██▄     ▀▀███▀▀▀    \n"); 
-    printf("\t\t\t\t\t\t ███    █▄  ▀███████████   ███    ███ ███     ███ ███            ███    █▄    ▐███  ▀███      ███    █▄ \n"); 
-    printf("\t\t\t\t\t\t ███    ███   ███    ███   ███    ███ ███ ▄█▄ ███ ███▌    ▄      ███    ███  ▄███     ███▄    ███    ███\n"); 
-    printf("\t\t\t\t\t\t ████████▀    ███    ███   ███    █▀   ▀███▀███▀  █████▄▄██  ██  ██████████ ████       ███▄   ██████████\n"); 
-    printf("\t\t\t\t\t\t              ███    ███                          ▀                                                       "); 
+    printf("\t\t\t\t\t\t ███    █▀    ███    ███   ███    ███ ███     ███ ███            ███    █▀     ███  ▐███      ███    █▀ \n");
+    printf("\t\t\t\t\t\t ███         ▄███▄▄▄▄██▀   ███    ███ ███     ███ ███           ▄███▄▄▄        ▀███▄███▀     ▄███▄▄▄    \n");
+    printf("\t\t\t\t\t\t ███        ▀▀███▀▀▀▀▀   ▀███████████ ███     ███ ███          ▀▀███▀▀▀        ████▀██▄     ▀▀███▀▀▀    \n");
+    printf("\t\t\t\t\t\t ███    █▄  ▀███████████   ███    ███ ███     ███ ███            ███    █▄    ▐███  ▀███      ███    █▄ \n");
+    printf("\t\t\t\t\t\t ███    ███   ███    ███   ███    ███ ███ ▄█▄ ███ ███▌    ▄      ███    ███  ▄███     ███▄    ███    ███\n");
+    printf("\t\t\t\t\t\t ████████▀    ███    ███   ███    █▀   ▀███▀███▀  █████▄▄██  ██  ██████████ ████       ███▄   ██████████\n");
+    printf("\t\t\t\t\t\t              ███    ███                          ▀                                                       ");
     printf("\n\n\n\n\n\n\n\n");
 }
 
-// Tela de controles
-void showControls() {
-    showTitle();
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t   W - para cima\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t   S - para baixo\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t   A - para esquerda\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t   D - para direita\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t   I - para interagir com item\n");
-    comebackMenu();
+// Cria o layout do mapa com paredes nas bordas
+void generateMapLayout(char map[10][10], int width, int height)
+{
+    playerX = 1;
+    playerY = height - 2;
+    enemyX = width / 2;
+    enemyY = height / 2;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (i == 0 || j == 0 || i == height - 1 || j == width - 1)
+                map[i][j] = '*';
+            else
+                map[i][j] = ' ';
+        }
+    }
 }
 
-// Tela de créditos
-void showCredits() {
-    showTitle();
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t  CREDITOS\n\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t  Dev 1: João Paulo Koury de Mendonça\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t  Dev 2: Pedro Antônio de Souza Fernandes Filho\n");
-    comebackMenu();
+// Mostra o mapa na tela
+void drawMap(char map[10][10], int width, int height)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            printf("%c ", map[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-// Geração e execução do mapa
-void generateMap(int length, int height) {
+// Movimento do jogador com WASD
+void movePlayer(char map[10][10], char input)
+{
+    if ((input == 'w' || input == 'W') && map[playerY - 1][playerX] != '*')
+        playerY--;
+    else if ((input == 's' || input == 'S') && map[playerY + 1][playerX] != '*')
+        playerY++;
+    else if ((input == 'a' || input == 'A') && map[playerY][playerX - 1] != '*')
+        playerX--;
+    else if ((input == 'd' || input == 'D') && map[playerY][playerX + 1] != '*')
+        playerX++;
+}
+
+// Movimento aleatório do inimigo
+void moveEnemy(char map[10][10], int width, int height)
+{
+    int dx = -1 + rand() % 3;
+    int dy = -1 + rand() % 3;
+    int newX = enemyX + dx;
+    int newY = enemyY + dy;
+
+    if (map[newY][newX] != '*' &&
+        newX > 0 && newX < width - 1 &&
+        newY > 0 && newY < height - 1)
+    {
+        enemyX = newX;
+        enemyY = newY;
+    }
+}
+
+// Fase com inimigo
+void generateMap()
+{
     system("cls");
     srand(time(NULL));
 
-    char map[length][height];
+    char map[10][10];
     char inputKey;
-    int i, j;
-    int playerX = 1, playerY = 8;
-    int enemieX = height / 2, enemieY = length / 2;
+    int width = 10, height = 10;
 
-    // Geração do mapa com paredes nas bordas
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < length; j++) {
-            if (i == 0 || j == 0 || i == height - 1 || j == length - 1)
+    generateMapLayout(map, width, height);
+    map[playerY][playerX] = '&';
+    map[enemyY][enemyX] = 'X';
+
+    while (1)
+    {
+        drawMap(map, width, height);
+        inputKey = getch();
+
+        map[playerY][playerX] = ' ';
+        map[enemyY][enemyX] = ' ';
+
+        movePlayer(map, inputKey);
+        moveEnemy(map, width, height);
+
+        map[playerY][playerX] = '&';
+        map[enemyY][enemyX] = 'X';
+
+        system("cls");
+    }
+}
+
+// Fase da vila (sem inimigo)
+void villageLevel()
+{
+    system("cls");
+    srand(time(NULL));
+
+    char map[10][10];
+    char inputKey;
+    int width = 10, height = 10;
+
+    playerX = 1;
+    playerY = height - 2;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (i == 0 || j == 0 || i == height - 1 || j == width - 1)
                 map[i][j] = '*';
             else
                 map[i][j] = ' ';
         }
     }
 
-    map[playerY][playerX] = '@';
-    map[enemieY][enemieX] = '&';
-
-    while (1) {
-        // Exibe o mapa
-        for (i = 0; i < height; i++) {
-            for (j = 0; j < length; j++) {
-                printf("%c ", map[i][j]);
-            }
-            printf("\n");
-        }
-
+    while (1)
+    {
+        map[playerY][playerX] = '&';
+        drawMap(map, width, height);
         inputKey = getch();
-
-        // Limpa posições anteriores
         map[playerY][playerX] = ' ';
-        map[enemieY][enemieX] = ' ';
-
-        // Movimento do jogador
-        if ((inputKey == 'w' || inputKey == 'W') && map[playerY - 1][playerX] != '*') playerY--;
-        else if ((inputKey == 'a' || inputKey == 'A') && map[playerY][playerX - 1] != '*') playerX--;
-        else if ((inputKey == 's' || inputKey == 'S') && map[playerY + 1][playerX] != '*') playerY++;
-        else if ((inputKey == 'd' || inputKey == 'D') && map[playerY][playerX + 1] != '*') playerX++;
-
-        // Movimento aleatório do inimigo
-        int deltaX = -1 + rand() % 3;
-        int deltaY = -1 + rand() % 3;
-        int newEnemieX = enemieX + deltaX;
-        int newEnemieY = enemieY + deltaY;
-
-        // Verifica se pode mover
-        if (map[newEnemieY][newEnemieX] != '*' &&
-            newEnemieX > 0 && newEnemieX < length - 1 &&
-            newEnemieY > 0 && newEnemieY < height - 1) {
-            enemieX = newEnemieX;
-            enemieY = newEnemieY;
-        }
-
-        // Atualiza mapa
-        map[playerY][playerX] = '@';
-        map[enemieY][enemieX] = '&';
+        movePlayer(map, inputKey);
         system("cls");
     }
 }
 
 // Menu principal
-int main() {
-    SetConsoleOutputCP(65001);
-    showTitle();
+int main()
+{
+    int loopMainMenu = 1;
 
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t  [1] INICIAR\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t  [2] CONTROLES\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t  [3] CREDITOS\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t  [4] SAIR\n");
-    printf("\t\t\t\t\t\t\t\t\t\t\t\t  Escolha uma opcao: ");
-    scanf("%d", &selection);
+    do
+    {
+        SetConsoleOutputCP(65001);
+        showTitle();
 
-    switch (selection) {
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t  [1] INICIAR\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t  [2] CONTROLES\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t  [3] CREDITOS\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t  [4] SAIR\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t  Escolha uma opcao: ");
+        scanf("%d", &selection);
+
+        switch (selection)
+        {
         case 1:
-            generateMap(10, 10);
+            villageLevel();
             break;
+
         case 2:
-            showControls();
+            showTitle();
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t   W - para cima\n");
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t   S - para baixo\n");
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t   A - para esquerda\n");
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t   D - para direita\n");
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t   I - para interagir com item\n");
+            printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t  ");
+            system("pause");
             break;
+
         case 3:
-            showCredits();
+            showTitle();
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t  CREDITOS\n\n");
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t  Dev 1: João Paulo Koury de Mendonça\n");
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t  Dev 2: Pedro Antônio de Souza Fernandes Filho\n");
+            printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t  ");
+            system("pause");
             break;
+
         case 4:
+            printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t  ");
             printf("Goodbye!\n");
+            loopMainMenu = 0;
             break;
+
         default:
-            printf("Invalid option.\n");
-    }
+            printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t  ");
+            printf("Opcao invalida.\n");
+            printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t  ");
+            system("pause");
+        }
+
+    } while (loopMainMenu == 1);
 
     return 0;
-}
-
-// Tela de retorno ao menu
-void comebackMenu() {
-    printf("\n\t\t\t\t\t\t\t\t\t\t\t\t  [1] Voltar ao menu\n");
-    printf("\n\t\t\t\t\t\t\t\t\t\t\t\t  [2] Sair do jogo\n");
-    printf("\n\t\t\t\t\t\t\t\t\t\t\t\t  Escolha uma opcao: ");
-    scanf(" %d", &selection);
-
-    if (selection == 1) {
-        system("cls");
-        main();
-    } else if (selection == 2) {
-        printf("\nFechando o jogo...\n");
-    } else {
-        printf("ERRO! Digito invalido, reinicie o jogo!");
-    }
 }
